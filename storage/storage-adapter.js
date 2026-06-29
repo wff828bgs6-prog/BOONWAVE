e4dbe572876ac6e23018951049d9b59288b165e8
wff828bgs6-prog <wff828bgs6@privaterelay.appleid.com>
2026-06-29 15:31:43 +0300

    Add media methods to storage contract

--- storage/storage-adapter.js
+++ storage/storage-adapter.js
@@ -6,6 +6,9 @@ export class StorageAdapter {
   async deleteCardWithLinks() { throw new Error('StorageAdapter.deleteCardWithLinks() is not implemented.'); }
   async saveLink() { throw new Error('StorageAdapter.saveLink() is not implemented.'); }
   async deleteLink() { throw new Error('StorageAdapter.deleteLink() is not implemented.'); }
+  async saveMedia() { throw new Error('StorageAdapter.saveMedia() is not implemented.'); }
+  async loadMedia() { throw new Error('StorageAdapter.loadMedia() is not implemented.'); }
+  async deleteMedia() { throw new Error('StorageAdapter.deleteMedia() is not implemented.'); }
   async saveSetting() { throw new Error('StorageAdapter.saveSetting() is not implemented.'); }
   async loadSetting() { throw new Error('StorageAdapter.loadSetting() is not implemented.'); }
 }
