aac3846ef70e372f43263825c2a0c8664ffef97b
wff828bgs6-prog <wff828bgs6@privaterelay.appleid.com>
2026-06-29 17:30:06 +0300

    Remove temporary adapter staging file

--- storage/indexeddb-adapter.new.js
+++
@@ -1,9 +0,0 @@
-import StorageAdapter from './storage-adapter.js';
-import db from './database.js';
-
-export class IndexedDBAdapter extends StorageAdapter {
-  constructor() {
-    super();
-    this.supportsAtomicCardGraphDelete = true;
-  }
-}
