09d811c1033c8f4b573a906e323fd3a84970885c
wff828bgs6-prog <wff828bgs6@privaterelay.appleid.com>
2026-06-29 15:09:08 +0300

    Extend storage contract for atomic card deletion

--- storage/storage-adapter.js
+++ storage/storage-adapter.js
@@ -3,6 +3,7 @@ export class StorageAdapter {
   async loadWorkspace() { throw new Error('StorageAdapter.loadWorkspace() is not implemented.'); }
   async saveCard() { throw new Error('StorageAdapter.saveCard() is not implemented.'); }
   async deleteCard() { throw new Error('StorageAdapter.deleteCard() is not implemented.'); }
+  async deleteCardWithLinks() { throw new Error('StorageAdapter.deleteCardWithLinks() is not implemented.'); }
   async saveLink() { throw new Error('StorageAdapter.saveLink() is not implemented.'); }
   async deleteLink() { throw new Error('StorageAdapter.deleteLink() is not implemented.'); }
   async saveSetting() { throw new Error('StorageAdapter.saveSetting() is not implemented.'); }
