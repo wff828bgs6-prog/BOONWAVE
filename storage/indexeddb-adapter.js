77571a1654eda0b57239935b7622244835997d48
wff828bgs6-prog <wff828bgs6@privaterelay.appleid.com>
2026-06-29 03:45:09 +0300

    feat(storage): add IndexedDB storage adapter

---
+++ storage/indexeddb-adapter.js
@@ -0,0 +1,38 @@
+import StorageAdapter from './storage-adapter.js';
+import db from './database.js';
+
+export class IndexedDBAdapter extends StorageAdapter {
+  async init() {
+    return db.initDB();
+  }
+
+  async loadWorkspace() {
+    return db.loadAllData();
+  }
+
+  async saveCard(card) {
+    return db.saveCard(card);
+  }
+
+  async deleteCard(id) {
+    return db.deleteCard(id);
+  }
+
+  async saveLink(link) {
+    return db.saveLink(link);
+  }
+
+  async deleteLink(id) {
+    return db.deleteLink(id);
+  }
+
+  async saveSetting(key, value) {
+    return db.saveSetting(key, value);
+  }
+
+  async loadSetting(key) {
+    return db.loadSetting(key);
+  }
+}
+
+export default new IndexedDBAdapter();
